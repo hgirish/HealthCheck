a71e1772818865b0e2fcb57e8accc72f42482fe8
Girish <girish@post.com>
2024-07-24 11:03:05 -0700

    HealthCheckComponent

---
+++ healthcheck.client/src/app/health-check/health-check.component.ts
@@ -0,0 +1,36 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-health-check',
+  templateUrl: './health-check.component.html',
+  styleUrl: './health-check.component.css'
+})
+export class HealthCheckComponent {
+  public result?: Result;
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.http.get<Result>(environment.baseUrl + 'api/health').subscribe({
+      next: (result) => this.result = result,
+      error: (error) => console.error(error),
+     complete: () => console.info('complete')
+    });
+  }
+
+}
+interface Result {
+  checks: Check[];
+  totalStatus: string;
+  totalResponseTime: number;
+}
+interface Check {
+  name: string;
+  responseTime: number;
+  status: string;
+  description: string;
+
+}
