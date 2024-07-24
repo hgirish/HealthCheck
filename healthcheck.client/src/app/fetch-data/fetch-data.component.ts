e0968a22a4c40f8ae8cb06a8e467f13724b1f0e6
Girish <girish@post.com>
2024-07-23 19:22:43 -0700

    Add project files.

---
+++ healthcheck.client/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html',
+  styleUrl: './fetch-data.component.css'
+})
+export class FetchDataComponent {
+  public forecasts: WeatherForecast[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+
+  getForecasts() {
+    this.http.get<WeatherForecast[]>(environment.baseUrl + 'api/weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+}
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
