244f504b4c199d13d4a0487ff5fb1823fc09b0ce
Girish <girish@post.com>
2024-07-30 21:46:21 -0700

    offline status

--- healthcheck.client/src/app/app.component.ts
+++ healthcheck.client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { ConnectionService, ConnectionServiceOptions } from 'ng-connection-service';
-import { map, Observable } from 'rxjs';
+import { ConnectionService, ConnectionServiceOptions, ConnectionState } from 'ng-connection-service';
+import { map, Observable, Subscription, tap } from 'rxjs';
 import { environment } from '../environments/environment';
 
 
@@ -13,15 +13,42 @@ import { environment } from '../environments/environment';
 export class AppComponent  {
   title = 'HealthCheck';
 
-  public isOffline: Observable<boolean>;
+  //public isOffline: Observable<boolean>;
+  status!: string;
+  currentState!: ConnectionState;
+  subscription = new Subscription();
 
   constructor(private connectionService: ConnectionService) {
+   
+        //this.isOffline = this.connectionService.monitor(options)
+     // .pipe(map(state => !state.hasNetworkConnection || state.hasInternetAccess));
+  }
+
+  ngOnInit(): void {
     const options: ConnectionServiceOptions = {
       enableHeartbeat: true,
       heartbeatUrl: environment.baseUrl + 'api/heartbeat',
       heartbeatInterval: 10000
     };
-    this.isOffline = this.connectionService.monitor(options)
-      .pipe(map(state => !state.hasNetworkConnection || state.hasInternetAccess));
+    this.subscription.add(
+      this.connectionService.monitor(options).pipe(
+        tap((newState: ConnectionState) => {
+          this.currentState = newState;
+
+          if (this.currentState.hasNetworkConnection) {
+            this.status = 'ONLINE';
+          
+          } else {
+            this.status = 'OFFLINE';
+            
+          }
+          console.log('status', this.status);
+        })
+      ).subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
